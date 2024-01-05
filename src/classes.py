d5b28f29568e54f03568c91b56b46a73f4346816
dyadyasawa <dyadyasawa@gmail.com>
2024-01-05 17:28:50 +0300

    Добавление функционала.

--- src/classes.py
+++ src/classes.py
@@ -3,22 +3,25 @@ from abc import ABC, abstractmethod
 
 
 class WorkApi(ABC):
-    """  """
+    """ Абстрактный класс для работы с API сайтов с вакансиями. """
 
     @abstractmethod
     def get_vacancies(self):
+        """ Абстрактный метод для подключения к API и получения вакансий. Реализуется в дочерних классах. """
         pass
 
 
 class HeadHunterApi(WorkApi):
-    """  """
+    """ Класс для работы с вакансиями через API сайта hh.ru. """
 
     def get_vacancies(self):
+        """ Метод для подключения к API и получения вакансий hh.ru. """
         pass
 
 
 class SuperJobApi(WorkApi):
-    """  """
+    """ Класс для работы с вакансиями через API сайта superjob.ru. """
 
     def get_vacancies(self):
+        """ Метод для подключения к API и получения вакансий superjob.ru."""
         pass
