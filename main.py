e1cb74e9c1abf4966717bd1895466445bb2e47d4
dyadyasawa <dyadyasawa@gmail.com>
2024-01-06 14:07:54 +0300

    Добавление функционала.

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
 
-import json
-import requests
-from pprint import pprint
-
-head_hunter_url = 'http://api.hh.ru/vacancies?text=""'
-#head_hunter_url = 'http://api.hh.ru/vacancies/7760476'
-
-response = requests.get(url=head_hunter_url).json()
-
-with open('data/hh_vacancies.json', 'w', encoding=('utf-8')) as file:
-    json.dump(response, file, ensure_ascii=False, indent=4)
-
-
-with open('data/hh_vacancies.json', 'r', encoding=('utf-8')) as file:
-    dict_info = json.load(file)
-
-pprint(dict_info)
-#print(len(dict_info['items']))
-
 def user_interaction():
     """ Функция для взаимодействия с пользователем """
     pass
--- src/classes.py
+++ src/classes.py
@@ -16,24 +16,33 @@ class WorkApi(ABC):
 
 class HeadHunterApi(WorkApi):
     """ Класс для работы с вакансиями через API сайта hh.ru. """
-
-    head_hunter_url = 'http://api.hh.ru/vacancies?text="Профессия"'
-
-
+    def __init__(self, prof):
+        self.prof = prof
 
     def get_vacancies(self):
         """ Метод для подключения к API и получения вакансий hh.ru. """
 
-        response = requests.get(url=HeadHunterApi.head_hunter_url).json()
+        response = requests.get(url=f'http://api.hh.ru/vacancies?text="{self.prof}"').json()
 
-        with open('data/hh_vacancies.json', 'w', encoding=('utf-8')) as file:
+        with open('../data/hh_vacancies.json', 'w', encoding=('UTF-8')) as file:
             json.dump(response, file, ensure_ascii=False, indent=4)
 
-        # with open('data/hh_vacancies.json', 'r', encoding=('utf-8')) as file:
-        #     dict_info = json.load(file)
-        #
-        # pprint(dict_info)
-        # print(len(dict_info['items']))
+        with open('../data/hh_vacancies.json', 'r', encoding=('UTF-8')) as file:
+            dict_info = json.load(file)
+
+        return dict_info
+
+    @staticmethod
+    def choice_dict(dict_for_choice):
+
+        dfc = dict_for_choice
+
+        print(f"Профессия: {dfc['items'][0]['name']}")
+        print(f"Зарплата: {dfc['items'][0]['salary']['from']}")
+        print(f"Валюта: {dfc['items'][0]['salary']['currency']}")
+        print(f"Место: {dfc['items'][0]['area']['name']}")
+        print(f"Занятость: {dfc['items'][0]['schedule']['name']}")
+        print(f"Условия: {dfc['items'][0]['snippet']['responsibility']}")
 
 
 class SuperJobApi(WorkApi):
@@ -42,3 +51,7 @@ class SuperJobApi(WorkApi):
     def get_vacancies(self):
         """ Метод для подключения к API и получения вакансий superjob.ru."""
         pass
+
+profession = input("По какой профессии вывести вакансии? >>>  ")
+p1 = HeadHunterApi(profession)
+print(p1.choice_dict(p1.get_vacancies()))
