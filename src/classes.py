a505c06e989c931e1ffe5f1bdc33bb49f188c218
dyadyasawa <dyadyasawa@gmail.com>
2024-01-06 12:20:12 +0300

    Добавление функционала.

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,8 @@
 
+import json
+import requests
 from abc import ABC, abstractmethod
+from pprint import pprint
 
 
 class WorkApi(ABC):
@@ -14,9 +17,23 @@ class WorkApi(ABC):
 class HeadHunterApi(WorkApi):
     """ Класс для работы с вакансиями через API сайта hh.ru. """
 
+    head_hunter_url = 'http://api.hh.ru/vacancies?text="Профессия"'
+
+
+
     def get_vacancies(self):
         """ Метод для подключения к API и получения вакансий hh.ru. """
-        pass
+
+        response = requests.get(url=HeadHunterApi.head_hunter_url).json()
+
+        with open('data/hh_vacancies.json', 'w', encoding=('utf-8')) as file:
+            json.dump(response, file, ensure_ascii=False, indent=4)
+
+        # with open('data/hh_vacancies.json', 'r', encoding=('utf-8')) as file:
+        #     dict_info = json.load(file)
+        #
+        # pprint(dict_info)
+        # print(len(dict_info['items']))
 
 
 class SuperJobApi(WorkApi):
