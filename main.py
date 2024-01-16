e807db5ea745ecdd9a5060b7a66cf17acc2d400f
dyadyasawa <dyadyasawa@gmail.com>
2024-01-16 21:43:32 +0300

    Легкие изменения.

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from src.classes import HeadHunterApi
 if __name__ == '__main__':
 
     profession = input("По какой профессии вывести вакансии? >>>  ")
-    area = int(input("В каком регионе? >>>  "))
+    area = input("В каком регионе? >>>  ")
     p1 = HeadHunterApi(profession, area)
 
-    #print(p1.get_vacancies())
-    print(p1.choice_dict(p1.get_vacancies()))
+    # print(p1.choice_dict(p1.get_vacancies()))
 
 
     def user_interaction():
--- src/classes.py
+++ src/classes.py
@@ -16,19 +16,20 @@ class WorkApi(ABC):
 
 class HeadHunterApi(WorkApi):
     """ Класс для работы с вакансиями через API сайта hh.ru. """
-    def __init__(self, prof: str, area: int):
+    def __init__(self, prof: str, area: str):
         self.prof = prof
         self.area = area
 
     def get_vacancies(self):
         """ Метод для подключения к API и получения вакансий hh.ru. """
 
-        url_hh = "http://api.hh.ru/vacancies"
-        params = {'text': f'name:{self.prof}',
-                  'area': 1,
-                  'page': 0}
-
-        response = requests.get(url_hh, params).json()
+        # url_hh = "http://api.hh.ru/vacancies"
+        # params = {'text': f'name:{self.prof}',
+        #           'area': 1,
+        #           'page': 0}
+        #
+        # response = requests.get(url_hh, params).json()
+        response = requests.get(f"http://api.hh.ru/vacancies?text=name:!{self.prof} AND !{self.area}").json()
 
 
         with open('data/hh_vacancies.json', 'w', encoding=('UTF-8')) as file:
@@ -41,18 +42,18 @@ class HeadHunterApi(WorkApi):
         return dict_info
 
 
-    @staticmethod
-    def choice_dict(dict_for_choice):
-
-        dfc = dict_for_choice
-
-        print(f"Профессия: {dfc['items'][0]['name']}")
-        print(f"Зарплата: {dfc['items'][0]['salary']['from']}")
-        print(f"Валюта: {dfc['items'][0]['salary']['currency']}")
-        print(f"Место: {dfc['items'][0]['area']['name']}")
-        print(f"Занятость: {dfc['items'][0]['schedule']['name']}")
-        print(f"Условия: {dfc['items'][0]['snippet']['responsibility']}")
-        print(f"Страница: {dfc['page']} из {dfc['pages']}")
+    # @staticmethod
+    # def choice_dict(dict_for_choice):
+    #
+    #     dfc = dict_for_choice
+    #
+    #     print(f"Профессия: {dfc['items'][0]['name']}")
+    #     print(f"Зарплата: {dfc['items'][0]['salary']['from']}")
+    #     print(f"Валюта: {dfc['items'][0]['salary']['currency']}")
+    #     print(f"Место: {dfc['items'][0]['area']['name']}")
+    #     print(f"Занятость: {dfc['items'][0]['schedule']['name']}")
+    #     print(f"Условия: {dfc['items'][0]['snippet']['responsibility']}")
+    #     print(f"Страница: {dfc['page']} из {dfc['pages']}")
 
 
 class SuperJobApi(WorkApi):
